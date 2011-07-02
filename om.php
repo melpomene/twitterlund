ec54463a399754e0dc9cd1428ce3a8f19a13a1f5
Fredrik Strandin <fredrik@empedokles.eu>
2011-07-02 15:05:34 +0200

    Added some initial design, needs some better colors and so. References #1

---
+++ om.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Twitterlund</title>
+	<meta name="author" content="Blekingska Hack day ">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<!-- Date: 2011-07-02 -->
+</head>
+<body>
+	<div id="menycontainer">
+		<div id="logga">
+			<h1>Twittterlund</h1>
+		</div>
+		<ul id="meny">
+			<li><a href="index.php">Flöde</a></li>
+			<li><a href="om.php" id="current">Om</a></li>
+		</ul>
+	</div>
+	<div id="content">
+		<h1>Om oss</h1>
+		<p>
+			Vi är ett gäng coola killar som tyckte att den här sidan fattades i världen, så vi gjorde den.
+		</p>
+	</div>
+</body>
+</html>
