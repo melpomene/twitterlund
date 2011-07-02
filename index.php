4df1a1d22dd3844822cf4728c598662b4a2d23f3
Fredrik Strandin <fredrik@empedokles.eu>
2011-07-02 15:12:47 +0200

    Failed to commit changes in index.php in previous commit

--- index.php
+++ index.php
@@ -6,9 +6,24 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Twitterlund</title>
 	<meta name="author" content="Blekingska Hack day ">
+	<link href="style.css" rel="stylesheet" type="text/css">
 	<!-- Date: 2011-07-02 -->
 </head>
 <body>
-
+	<div id="menycontainer">
+		<div id="logga">
+			<h1>Twittterlund</h1>
+		</div>
+		<ul id="meny">
+			<li><a href="index.php" id="current">Flöde</a></li>
+			<li><a href="om.php">Om</a></li>
+		</ul>
+	</div>
+	<div id="content">
+		<h1>Det här händer i Lund:</h1>
+		<p>
+			En fet feed!
+		</p>
+	</div>
 </body>
 </html>
